8ab9809be5d0ab428bf64ab5ef62822b22e0c0f8
Raygama <daffaraygama55@gmail.com>
2023-07-28 20:30:41 +0700

    try 0.0.0.0

--- config/db.go
+++ config/db.go
@@ -11,7 +11,7 @@ import (
 func ConnectDatabase() *gorm.DB {
 	username := "root"
 	password := "Antimaling2@"
-	host := "tcp(127.0.0.1:3306)"
+	host := "tcp(0.0.0.0:3306)"
 	database := "db_game"
 
 	dsn := fmt.Sprintf("%v:%v@%v/%v?charset=utf8mb4&parseTime=True&loc=Local", username, password, host, database)
