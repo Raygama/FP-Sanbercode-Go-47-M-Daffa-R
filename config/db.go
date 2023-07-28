abd969bd378cdb33f3a8e9466bc191fba93bfeaa
Raygama <daffaraygama55@gmail.com>
2023-07-28 23:26:02 +0700

    db.go changed 3

--- config/db.go
+++ config/db.go
@@ -10,11 +10,11 @@ import (
 )
 
 func ConnectDatabase() *gorm.DB {
-	username := utils.Getenv("root", "root")
-	password := utils.Getenv("8J2b5pX9RygNKe5iXC0s", "Antimaling2@")
-	host := utils.Getenv("containers-us-west-116.railway.app", "0.0.0.0")
-	port := utils.Getenv("7659", "3306")
-	database := utils.Getenv("railway", "db_game")
+	username := utils.Getenv("MYSQLDATABASE", "root")
+	password := utils.Getenv("MYSQLPASSWORD", "8J2b5pX9RygNKe5iXC0s")
+	host := utils.Getenv("MYSQLHOST", "containers-us-west-116.railway.app")
+	port := utils.Getenv("MYSQLPORT", "7659")
+	database := utils.Getenv("MYSQLDATABASE", "railway")
 
 	dsn := fmt.Sprintf("%v:%v@tcp(%v:%v)/%v?charset=utf8mb4&parseTime=True&loc=Local", username, password, host, port, database)
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
