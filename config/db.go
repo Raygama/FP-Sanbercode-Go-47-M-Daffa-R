f6945936d3886347e58cd316d3a29d05ad46cf2b
Raygama <daffaraygama55@gmail.com>
2023-07-28 22:44:46 +0700

    changed db.go 2

--- config/db.go
+++ config/db.go
@@ -5,17 +5,18 @@ import (
 	"gorm.io/gorm"
 
 	"Final/models"
+	"Final/utils"
 	"fmt"
 )
 
 func ConnectDatabase() *gorm.DB {
-	username := "root"
-	password := "8J2b5pX9RygNKe5iXC0s"
-	host := "containers-us-west-116.railway.app"
-	database := "railway"
+	username := utils.Getenv("root", "root")
+	password := utils.Getenv("8J2b5pX9RygNKe5iXC0s", "Antimaling2@")
+	host := utils.Getenv("containers-us-west-116.railway.app", "127.0.0.1")
+	port := utils.Getenv("7659", "3306")
+	database := utils.Getenv("railway", "db_game")
 
-	dsn := fmt.Sprintf("%v:%v@%v/%v?charset=utf8mb4&parseTime=True&loc=Local", username, password, host, database)
-
+	dsn := fmt.Sprintf("%v:%v@tcp(%v:%v)/%v?charset=utf8mb4&parseTime=True&loc=Local", username, password, host, port, database)
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
